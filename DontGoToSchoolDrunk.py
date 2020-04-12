9800a032954d8be7ff711cd0b35ea370df9c4986
Ldarrah <61467887+Ldarrah@users.noreply.github.com>
2020-04-12 11:10:07 -0400

    final code

---
+++ DontGoToSchoolDrunk.py
@@ -0,0 +1,7 @@
+for x in range(1,11):
+    if (x%2)== 1:
+        what = 'odd'
+        print(what)
+    else:
+        what = 'even'
+        print(what)
--- Lisa.py
+++ Lisa.py
@@ -1,1 +1,22 @@
-#Hello World
+faveLanguage = "Spanish"
+faveInstrument = "fiddle"
+faveDrink = "iced tea"
+faveBook = "Atlas Shrugged"
+coolLanguages = ["Afrikaans", "Finnish", "Latin", "American Sign Language"]
+coolInstruments = ["didgeridoo", "pan pipes", "church organ", "fiddle"]
+coolDrinks = ["civet coffee", "kombucha", "kefir"]
+coolBooks = ["The Catcher in the Rye", "Atlas Shrugged", "Anna Karenina", "Hamlet"]
+if faveBook in coolBooks:
+    print("Nice book choice!")
+    if faveLanguage in coolLanguages:
+        print("Let's practice together sometime!")
+        if faveInstrument in coolInstruments:
+            print("I play that, too!")
+    else:
+        print("You should learn a new language.")
+        if faveDrink in coolDrinks or faveInstrument in coolInstruments:
+            print("You have good taste!")
+        else:
+            print("What cool things are you into?")
+else:
+    print("See you later...")
