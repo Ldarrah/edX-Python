cb61e509202b0da6046b850df711b149a91d0e83
Ldarrah <61467887+Ldarrah@users.noreply.github.com>
2020-04-12 13:13:18 -0400

    final submission

--- Valentine321.py
+++ Valentine321.py
@@ -1,18 +1,18 @@
-years = 2
+years = 0
 months = 5
-days = 24
+days = 0
 
-#You may modify the lines of code above, but don't move them!
-#When you Submit your code, we'll change these lines to
-#assign different values to the variables.
+# You may modify the lines of code above, but don't move them!
+# When you Submit your code, we'll change these lines to
+# assign different values to the variables.
 
-#Ever been at a loss for what to do for your significant
-#other for Valentine's Day? Let's right some code to generate
-#a gift recommendation!
+# Ever been at a loss for what to do for your significant
+# other for Valentine's Day? Let's right some code to generate
+# a gift recommendation!
 #
-#The variables above give the length of the relationship in
-#number of years, months, and days. Add some code below to
-#print a gift recommendation based on these values:
+# The variables above give the length of the relationship in
+# number of years, months, and days. Add some code below to
+# print a gift recommendation based on these values:
 #
 # - If you've been dating for at least 4 years, give them a
 #   dog ("dog").
@@ -25,20 +25,22 @@ days = 24
 # - If aren't actually dating, go big or go home: give them
 #   a yacht to sail away together ("yacht").
 #
-#Note that no matter what, you should only print one gift.
+# Note that no matter what, you should only print one gift.
 
 
-#Add your code here!
-if years >= 4:
+# Add your code here!
+if years == 0 and months == 0 and days == 0:
+    gift = "yacht"
+elif years >= 4:
     gift = "dog"
 elif years >= 1:
-    if years < 4:
-        gift = "watch"
+    gift = "watch"
 elif years < 1:
     if months >= 6:
         gift = "concert tickets"
-    elif months < 6 and days > 1:
-                gift = "candy"
-else:
-    gift = "yacht"
-print (gift)
+    elif months < 6 or (months == 0 and days >= 1):
+        gift = "candy"
+    else:
+        gift = "yacht"
+
+print(gift)
